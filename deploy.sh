2a327cb1273460cf41824fe9724c94b3094bdfe7
chundo <segundo.espana1@gmail.com>
2022-06-03 23:44:19 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 #1 Creo un usuario
 
 adduser deploy &&
-adduser deploy sudo
+adduser deploy sudo &&
 
 #2 Actualizar OS, instalar dependencias y ruby
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - &&
@@ -13,24 +13,24 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 sudo add-apt-repository ppa:chris-lea/redis-server &&
 sudo apt-get install -y nodejs yarn &&
 sudo apt-get update &&
-sudo apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev dirmngr gnupg apt-transport-https ca-certificates redis-server redis-tools nodejs yarn
+sudo apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev dirmngr gnupg apt-transport-https ca-certificates redis-server redis-tools nodejs yarn &&
 
 #3 Instalar rbenv
-# rm *.zip &&
-#rm -r ~/.rbenv &&
-#rm -r rbenv-master &&
+rm *.zip &&
+# rm -r ~/.rbenv &&
+# rm -r rbenv-master &&
 
 # git clone https://github.com/rbenv/rbenv.git ~/.rbenv &&
 wget https://github.com/rbenv/rbenv/archive/refs/heads/master.zip  &&
 unzip master.zip &&
 mkdir ~/.rbenv &&
-#mv rbenv-master/* /root/.rbenv &&
+# mv rbenv-master/* /root/.rbenv &&
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc &&
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc &&
 
-#git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build &&
+# git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build &&
 rm *.zip &&
-#rm -r ruby-build-master &&
+# rm -r ruby-build-master &&
 wget https://github.com/rbenv/ruby-build/archive/refs/heads/master.zip &&
 unzip master.zip &&
 mkdir ~/.rbenv/plugins &&
@@ -38,9 +38,9 @@ mkdir ~/.rbenv/plugins/ruby-build &&
 mv ruby-build-master/* ~/.rbenv/plugins/ruby-build &&
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc &&
 
-#git clone https://github.com/rbenv/rbenv-vars.git ~/.rbenv/plugins/rbenv-vars &&
+# git clone https://github.com/rbenv/rbenv-vars.git ~/.rbenv/plugins/rbenv-vars &&
 rm *.zip &&
-#rm -r rbenv-vars-master &&
+# rm -r rbenv-vars-master &&
 wget https://github.com/rbenv/rbenv-vars/archive/refs/heads/master.zip &&
 unzip master.zip &&
 mkdir ~/.rbenv/plugins/rbenv-vars &&
