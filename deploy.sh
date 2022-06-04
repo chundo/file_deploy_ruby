0f89902b15c32eefced3c6f1dabe139d7d9261ec
chundo <segundo.espana1@gmail.com>
2022-06-04 17:50:35 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 
 #1 Creo un usuario
 
-adduser deploy &&
-adduser deploy sudo &&
+# adduser deploy &&
+# adduser deploy sudo &&
 
 #2 Actualizar OS, instalar dependencias y ruby
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - &&
@@ -13,10 +13,11 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 sudo add-apt-repository ppa:chris-lea/redis-server &&
 sudo apt-get install -y nodejs yarn &&
 sudo apt-get update &&
+sudo apt-get install -y unzip &&
 sudo apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev dirmngr gnupg apt-transport-https ca-certificates redis-server redis-tools nodejs yarn &&
 
 #3 Instalar rbenv
-rm *.zip &&
+#rm *.zip &&
 #rm -r ~/.rbenv &&
 #rm -r rbenv-master &&
 
