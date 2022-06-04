72b1e8e8607b102deff52fe9f4819775741b1280
chundo <segundo.espana1@gmail.com>
2022-06-03 21:44:30 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,7 @@ rm *.zip &&
 rm -r ~/.rbenv &&
 # git clone https://github.com/rbenv/rbenv.git ~/.rbenv &&
 wget https://github.com/rbenv/rbenv/archive/refs/heads/master.zip  &&
+unzip master.zip &&
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc &&
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc &&
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build &&
